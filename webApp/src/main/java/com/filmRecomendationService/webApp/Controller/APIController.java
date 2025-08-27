12a2907533ff3869c73af56f38003b1003b540cb
IvanDrv <ivander05@mail.ru>
2025-08-27 20:17:19 +0300

    Parametrized GET method fixed

--- webApp/src/main/java/com/filmRecomendationService/webApp/Controller/APIController.java
+++ webApp/src/main/java/com/filmRecomendationService/webApp/Controller/APIController.java
@@ -1,27 +1,29 @@
 package com.filmRecomendationService.webApp.Controller;
 
 import com.filmRecomendationService.webApp.DataTransferObjects.RecommendationRequestDTO;
-import com.filmRecomendationService.webApp.Entity.Film;
 import com.filmRecomendationService.webApp.Service.RecommendationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequestMapping(path = "api/", produces = "text/plain;charset=UTF-8")
-@CrossOrigin
+@CrossOrigin//(origins = "http://127.0.0.1:5500")
 public class APIController {
     @Autowired
     private RecommendationService model;
 
-    @GetMapping("recommendationList")
-    List<Film> getRecommendationList(@RequestBody RecommendationRequestDTO request) {
-        return model.handleRecommendationRequest(request);
-    }
 
     @GetMapping("recommendationString")
-    String getRecommendationString(@RequestBody RecommendationRequestDTO request) {
+    String getRecommendationString(
+            @RequestParam String userMood,
+            @RequestParam Integer startYear,
+            @RequestParam Integer endYear,
+            @RequestParam String genre
+    ) {
+        RecommendationRequestDTO request = new RecommendationRequestDTO(userMood,
+                startYear,
+                endYear,
+                genre);
         return model.makeStringRecommendation(request, "en");
     }
 
@@ -37,6 +39,3 @@ public class APIController {
                 " end year = " + request.getEndYear() + " genre = " + request.getGenre() + ".";
     }
 }
-
-// tested with
-// curl -X GET -H "Content-Type: Application/json" -d {\"userMood\":\"happy\",\"startYear\":\"1990\",\"endYear\":\"2100\",\"genre\":\"comedy\"}  http://localhost:8080/api/testJSON
