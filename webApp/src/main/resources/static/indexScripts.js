bbbaa401a19c58ed8a3e47b0000510402f2c8c63
IvanDrv <ivander05@mail.ru>
2025-08-28 10:11:57 +0300

    Recommendation request handling function

--- webApp/src/main/resources/static/indexScripts.js
+++ webApp/src/main/resources/static/indexScripts.js
@@ -1,5 +1,9 @@
 const responseField = document.getElementById("responseField");
+const startYearField = document.getElementById("StartYearField");
+const endYearField = document.getElementById("EndYearField");
 
+const moodSelector = document.getElementById("MoodSelector");
+const genreSelector = document.getElementById("GenreSelector");
 
 async function test() {
     const response = await fetch("http://192.168.0.106:8080/api/test", {
@@ -8,6 +12,34 @@ async function test() {
     );
     try{
         if (!response.ok) throw new Error('Ошибка сервера');
+
+        const result = await response.text();
+        responseField.textContent = result;
+
+    } catch (err){
+        console.log(err);
+    }
+    
+}
+
+async function askRecommendation() {
+        const params = new URLSearchParams({
+        userMood: moodSelector.value,
+        startYear: startYearField.value,
+        endYear: endYearField.value,
+        genre: genreSelector.value
+    });
+
+    const response = await fetch(`http://192.168.0.100:8080/api/recommendationString?${params.toString()}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    try{
+
+        if (!response.ok) throw new Error("Ошибка сервера");
 
         const result = await response.text();
         responseField.textContent = result;
