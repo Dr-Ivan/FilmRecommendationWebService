dc4de0cda0f4a8a65714475561b4436621198ea6
IvanDrv <ivander05@mail.ru>
2025-12-24 18:06:19 +0300

    Scripts for web page

--- webApp/src/main/resources/static/indexScripts.js
+++ webApp/src/main/resources/static/indexScripts.js
@@ -6,7 +6,8 @@ const moodSelector = document.getElementById("MoodSelector");
 const genreSelector = document.getElementById("GenreSelector");
 
 async function test() {
-    const response = await fetch("http://192.168.0.106:8080/api/test", {
+    //const response = await fetch("http://192.168.0.106:8080/api/test", {
+    const response = await fetch("http://localhost:8080/api/test", {
         method: 'GET'
     }
     );
@@ -23,14 +24,15 @@ async function test() {
 }
 
 async function askRecommendation() {
-        const params = new URLSearchParams({
+    const params = new URLSearchParams({
         userMood: moodSelector.value,
         startYear: startYearField.value,
         endYear: endYearField.value,
         genre: genreSelector.value
     });
 
-    const response = await fetch(`http://192.168.0.100:8080/api/recommendationString?${params.toString()}`, {
+    //const response = await fetch(`http://192.168.0.104:8080/api/recommendationString?${params.toString()}`, {
+    const response = await fetch(`http://localhost:8080/api/recommendationString?${params.toString()}`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json'
